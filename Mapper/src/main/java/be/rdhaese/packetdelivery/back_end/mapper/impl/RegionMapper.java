43d98e3e43bfda8f73f376120ee7f8d1dd690110
Robin D'Haese <robin.d'haese@realdolmen.com>
2015-12-30 11:17:04 +0100

    added mappers and company details from prototype

---
+++ Mapper/src/main/java/be/rdhaese/packetdelivery/back_end/mapper/impl/RegionMapper.java
@@ -0,0 +1,38 @@
+package be.rdhaese.packetdelivery.back_end.mapper.impl;
+
+import be.rdhaese.packetdelivery.back_end.mapper.AbstractMapper;
+import be.rdhaese.project.dto.RegionDTO;
+import be.rdhaese.project.model.Region;
+import be.rdhaese.project.service.RegionsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created on 25/12/2015.
+ *
+ * @author Robin D'Haese
+ */
+@Component
+public class RegionMapper extends AbstractMapper<Region, RegionDTO> {
+    @Autowired
+    private RegionsService regionsService;
+
+    @Override
+    public Region mapToBus(RegionDTO dto) {
+        if (dto == null){
+            return null;
+        }
+        return regionsService.getRegionFor(dto.getCode());
+    }
+
+    @Override
+    public RegionDTO mapToDto(Region busObj) {
+        if (busObj == null){
+            return null;
+        }
+        RegionDTO regionDTO = new RegionDTO();
+        regionDTO.setName(busObj.getName());
+        regionDTO.setCode(busObj.getRegionCode());
+        return regionDTO;
+    }
+}
---
+++ Model/src/main/java/be/rdhaese/packetdelivery/back_end/model/company_details/FaxEntry.java
@@ -0,0 +1,61 @@
+package be.rdhaese.packetdelivery.back_end.model.company_details;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+/**
+ * Created on 27/12/2015.
+ *
+ * @author Robin D'Haese
+ */
+@XmlRootElement (name = "fax")
+public class FaxEntry implements Serializable{
+    private String title;
+    private String number;
+
+    public FaxEntry() {
+    }
+
+    public FaxEntry(String title, String number) {
+        this.title = title;
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FaxEntry faxEntry = (FaxEntry) o;
+
+        if (title != null ? !title.equals(faxEntry.title) : faxEntry.title != null) return false;
+        return !(number != null ? !number.equals(faxEntry.number) : faxEntry.number != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (number != null ? number.hashCode() : 0);
+        return result;
+    }
+
+    @XmlElement (name = "fax-title")
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @XmlElement (name = "fax-number")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
