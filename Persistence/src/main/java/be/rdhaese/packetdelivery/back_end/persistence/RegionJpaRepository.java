94b246d1d79f099ae5917a79b8d8ecc82ebc1b36
Robin D'Haese <robin.d'haese@realdolmen.com>
2015-12-30 11:18:20 +0100

    added repositories from prototype

---
+++ Persistence/src/main/java/be/rdhaese/packetdelivery/back_end/persistence/RegionJpaRepository.java
@@ -0,0 +1,18 @@
+package be.rdhaese.packetdelivery.back_end.persistence;
+
+import be.rdhaese.project.model.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created on 25/12/2015.
+ *
+ * @author Robin D'Haese
+ */
+@Repository
+public interface RegionJpaRepository extends JpaRepository<Region, Long> {
+
+    @Query("SELECT r FROM Region r WHERE r.regionCode = ?1")
+    Region getRegionFor(String regionCode);
+}
